6519b9736dc6551666d502e4b462b57b2c7813e8
marona42 <marona42@naver.com>
2020-11-02 21:06:19 +0900

    complete 1st job

--- 1/master-worker.c
+++ 1/master-worker.c
@@ -7,16 +7,17 @@
 #include <signal.h>
 #include <wait.h>
 #include <pthread.h>
+int DEBUG=0;
 
 int item_to_produce, curr_buf_size;
 int total_items, max_buf_size, num_workers, num_masters;
 pthread_mutex_t mslock = PTHREAD_MUTEX_INITIALIZER;
-//pthread_mutex_t mslock = PTHREAD_MUTEX_INITIALIZER;
 int *buffer;
 
 void print_produced(int num, int master) {
 
   printf("Produced %d by master %d\n", num, master);
+  if(DEBUG) printf(" at buffer %d/%d\n", curr_buf_size,max_buf_size);
 }
 
 void print_consumed(int num, int worker) {
@@ -31,16 +32,14 @@ void print_consumed(int num, int worker) {
 void *generate_requests_loop(void *data)
 {
   int thread_id = *((int *)data);
-  printf("master thread %d is started\n",thread_id);
   while(1)
   {
-    sleep(1);
     pthread_mutex_lock(&mslock);
     if(item_to_produce >= total_items) {
       pthread_mutex_unlock(&mslock);
       break;
     }
-    if(curr_buf_size > max_buf_size)
+    if(curr_buf_size >= max_buf_size)
     {
       pthread_mutex_unlock(&mslock);
       continue;
@@ -49,10 +48,8 @@ void *generate_requests_loop(void *data)
     print_produced(item_to_produce, thread_id);
     item_to_produce++;
     pthread_mutex_unlock(&mslock);
-  //  printf("*thread %d beep*\n",thread_id);
   }
-  printf("Mthread %d is end\n",thread_id);
-  pthread_exit(NULL);
+  //pthread_exit(NULL);
   return 0;
 }
 
@@ -62,10 +59,8 @@ void *use_requests_loop(void *data)
 {
   int thread_id = *((int *)data);
   int consuming_target;
-  printf("worker thread %d is started\n",thread_id);
   while(1)
   {
-    sleep(1);
     pthread_mutex_lock(&mslock);
     if(curr_buf_size <= 0)
     {
@@ -80,13 +75,11 @@ void *use_requests_loop(void *data)
         continue;
       }
     }
-    consuming_target = buffer[curr_buf_size--];
+    consuming_target = buffer[--curr_buf_size];
     print_consumed(consuming_target, thread_id);
     pthread_mutex_unlock(&mslock);
-  //  printf("*thread %d beep*\n",thread_id);
   }
-  printf("Wthread %d is end\n",thread_id);
-  pthread_exit(NULL);
+  //pthread_exit(NULL);
   return 0;
 }
 
@@ -100,24 +93,17 @@ int main(int argc, char *argv[])
   
   int i;
   
-  if (argc == 1)
-  {
-    num_masters  = 2;      // 숫자를 생성할 마스터 스레드의 수 P
-    num_workers  = 2;      // 생성한 숫자를 소비하는 워커 스레드 수 C
-    total_items  = 4;      // 생성할 숫자 M
-    max_buf_size = 3;     // 생성된 숫자 저장할 버퍼 최대 크기 N
-  }
-  else if (argc < 5) {
+  if (argc < 5) {
     printf("%s #total_items #max_buf_size #num_workers #masters e.g. %s 10000 1000 4 3\n",argv[0],argv[0]);
     exit(1);
   }
   else {
-    total_items = atoi(argv[1]);      // 생성할 숫자 M
+    total_items = atoi(argv[1]);      // 생성할 숫자의 범위 M
     max_buf_size = atoi(argv[2]);     // 생성된 숫자 저장할 버퍼 최대 크기 N
     num_workers = atoi(argv[3]);      // 생성한 숫자를 소비하는 워커 스레드 수 C
     num_masters = atoi(argv[4]);      // 숫자를 생성할 마스터 스레드의 수 P
   }
-    
+  //if (argc > 5) DEBUG=1;   //버퍼 크기 확인용
 
    buffer = (int *)malloc (sizeof(int) * max_buf_size);
 
@@ -128,9 +114,8 @@ int main(int argc, char *argv[])
     master_thread_id[i] = i;
 
   for (i = 0; i < num_masters; i++)
-  {
     pthread_create(&master_thread[i], NULL, generate_requests_loop, (void *)&master_thread_id[i]);
-  }
+
   //create worker consumer threads
   worker_thread_id = (int *)malloc(sizeof(int) * num_workers);
   worker_thread = (pthread_t *)malloc(sizeof(pthread_t) * num_workers);
@@ -138,26 +123,27 @@ int main(int argc, char *argv[])
     worker_thread_id[i] = i;
       
   for (i = 0; i < num_workers; i++)
-    pthread_create(&worker_thread[i], NULL, use_requests_loop, (void *)&master_thread_id[i]);
+    pthread_create(&worker_thread[i], NULL, use_requests_loop, (void *)&worker_thread_id[i]);
   
   //wait for all threads to complete
   for (i = 0; i < num_masters; i++)
-    {
-      pthread_join(master_thread[i], NULL);
-      printf("master %d joined\n", i);
-      pthread_join(worker_thread[i], NULL);
-      printf("worker %d joined\n", i);
-    }
-  
-  //FIXME: 디버그용: 만든 숫자 확인하기.
+  {
+    pthread_join(master_thread[i], NULL);
+    printf("master %d joined\n", i);
+  }
+  for (i = 0; i < num_workers; i++)
+  {
+    pthread_join(worker_thread[i], NULL);
+    printf("worker %d joined\n", i);
+  }
 
   /*----Deallocating Buffers---------------------*/
   free(buffer);
   free(master_thread_id);
   free(master_thread);
-  
+  free(worker_thread_id);
+  free(worker_thread);
   pthread_mutex_destroy(&mslock);
-  //pthread_mutex_destroy(&mslock);
 
   return 0;
 }
