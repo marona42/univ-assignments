477a9203bf89a1c1a5c023a3a72cf11e9ff707a6
marona42 <marona42@naver.com>
2020-11-04 05:03:34 +0900

    scatch rwlockw

--- 2/rw_lock-w-test.c
+++ 2/rw_lock-w-test.c
@@ -6,23 +6,67 @@ void init_rwlock(struct rw_lock * rw)
   rw->users=0;
   rw->waitings_r=0;
   rw->waitings_w=0;
+  pthread_spin_init(&rw->lockpin,0);
 }
 
 void r_lock(struct rw_lock * rw)  //이전 reading lock 스레드와 동시에 획득 가능.
 {
   //	Write the code for aquiring read-write lock by the reader.
   if(rw->status <= IDLE)
-    rw->status=READ;
+  {
+    pthread_spin_lock(&rw->lockpin);
+    rw->status=READ; rw->users++;
+    //printf("->![%d]\n",rw->users);
+    pthread_spin_unlock(&rw->lockpin);
+  }
   else if(rw->status >= WRITE || rw->waitings_w)    //이미 write중인 스레드가 존재함. read lock 획득 불가.
-    {rw->waitings_r++; while(rw->waitings_w || rw->status>=WRITE); rw->status=READ; rw->waitings_r--;}
-  rw->users++;  //read하는 스레드 수 증가.
+  {
+    rw->waitings_r++;
+    while(1)
+    {
+      pthread_spin_lock(&rw->lockpin);
+      if(rw->status<=READ && !rw->waitings_w)
+      {
+        //printf("%d[%d]->",rw->status,rw->waitings_w);
+        rw->status=READ; rw->waitings_r--;  rw->users++; 
+        //printf("%d(%d)\n",rw->status,rw->users);
+        pthread_spin_unlock(&rw->lockpin);
+        break;
+      }
+      pthread_spin_unlock(&rw->lockpin);
+    }
+  }
+  else   //이미 read중인 스레드가 존재함. read 획득 가능.
+  {
+    rw->waitings_r++;
+    while(1)
+    {
+      pthread_spin_lock(&rw->lockpin);
+      if(rw->status<=READ && !rw->waitings_w)
+      {
+        //printf("%d[%d]->",rw->status,rw->waitings_w);
+        rw->status=READ; rw->waitings_r--;  rw->users++; 
+        //printf("%d(%d)\n",rw->status,rw->users);
+        pthread_spin_unlock(&rw->lockpin);
+        break;
+      }
+      pthread_spin_unlock(&rw->lockpin);
+    }
+  }
 }
 
 void r_unlock(struct rw_lock * rw)
 {
   //	Write the code for releasing read-write lock by the reader.
+  pthread_spin_lock(&rw->lockpin);
   rw->users--;
+  //printf("[%d]=>%d\n",rw->users,rw->status);
   if(rw->users<=0) rw->status = IDLE;   //더 이상 이 lock을 read하는 스레드가 없음.
+  pthread_spin_unlock(&rw->lockpin);
+
+  
+  //rw->users--;
+  //if(rw->users<=0) rw->status = IDLE;   //더 이상 이 lock을 read하는 스레드가 없음.
 }
 
 void w_lock(struct rw_lock * rw)  //w은 r이 끝날때까지 대기해야함.
@@ -31,14 +75,30 @@ void w_lock(struct rw_lock * rw)  //w은 r이 끝날때까지 대기해야함.
   if(rw->status <= IDLE && !rw->waitings_w)
     rw->status=WRITE;
   else                           //이미 lock중인 스레드가 존재함. write lock 획득 불가.
-  { rw->waitings_w++;  while(rw->status > IDLE) usleep(1); rw->status=WRITE; rw->waitings_w--; }
-    //{rw->waitings_w++;  while(1) if(rw->status <= IDLE){rw->waitings_w--; rw->status=WRITE; break;} }
-  rw->users++;
+  {
+    rw->waitings_w++;
+    while(1)
+    {
+      pthread_spin_lock(&rw->lockpin);
+      if(rw->status <= IDLE)
+      {
+        //printf("%d",rw->status);
+        rw->status=WRITE; rw->waitings_w--; rw->users++;
+        //printf("->%d(%d)\n",rw->status,rw->users);
+        pthread_spin_unlock(&rw->lockpin);
+        break;
+      }
+      pthread_spin_unlock(&rw->lockpin);
+    }
+  }
 }
 
 void w_unlock(struct rw_lock * rw)
 {
   //	Write the code for releasing read-write lock by the writer.
+  pthread_spin_lock(&rw->lockpin);
+  //printf("[%d]=>%d\n",rw->users,rw->status);
   rw->users--;
-  if(rw->users<=0) rw->status=IDLE;
+  if(rw->users<=0) rw->status=IDLE;  
+  pthread_spin_unlock(&rw->lockpin);
 }
