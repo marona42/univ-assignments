ec2b0fa53b2930e7eda43a6d54ffe7caf3340edb
marona42 <marona42@naver.com>
2020-11-20 14:27:47 +0900

    oh, I got it!

--- alloc/alloc.c
+++ alloc/alloc.c
@@ -2,11 +2,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <errno.h>
 
-
+struct memmanger_s{
+    void *prologue;
+    void *bookmark;
+} mmanager_static;
 int init_alloc()
 {
-
+    //mmap(void *addr, size_t len, int prot, int flag,int fd, off_t off): 시작주소(0-운영체제선택)/파일영역의 크기/보호방식/매핑영역특성/매핑할 파일(오픈 필요)/시작위치 파일오프셋
+    if((mmanager_static.prologue=mmap(0,PAGESIZE,(PROT_WRITE|PROT_READ),(MAP_ANONYMOUS|MAP_PRIVATE),-1,0))==MAP_FAILED) return errno;
+    return 0;
 }
 int cleanup()
 {
