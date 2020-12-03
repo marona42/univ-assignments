30f534559ebd7db008a96b646113f0076957bdaf
marona42 <marona42@naver.com>
2020-12-03 21:07:27 +0900

    read

--- ssufs-ops.c
+++ ssufs-ops.c
@@ -12,7 +12,7 @@ int ssufs_allocFileHandle() {
 }
 
 int ssufs_create(char *filename){
-	/*1 TODO: filename으로 파일 생성, inode 할당-반환, 초기화 필요. 동일이름의 파일이 시스템에 존재하지 않아야 함*/
+	/*1 MEMO: filename으로 파일 생성, inode 할당-반환, 초기화 필요. 동일이름의 파일이 시스템에 존재하지 않아야 함*/
 
 	struct superblock_t *superblock = (struct superblock_t *) malloc(sizeof(struct superblock_t));
 	struct inode_t *tmp = (struct inode_t *) malloc(sizeof(struct inode_t));
@@ -61,7 +61,7 @@ int ssufs_create(char *filename){
 }
 
 void ssufs_delete(char *filename){
-	/*2 TODO: filname 삭제, inode와 같은 관련 리소스 해제. close 안해도 삭제 가능 */
+	/*2 MEMO: filname 삭제, inode와 같은 관련 리소스 해제. close 안해도 삭제 가능 */
 	//THINK: 데이터블록 freelist를 지우긴 함
 	int inode=open_namei(filename);
 	if(inode<0) return -1;	//filename인 파일이 없음
@@ -74,10 +74,11 @@ void ssufs_delete(char *filename){
 		if(tmp->direct_blocks[i]) ssufs_freeDataBlock(tmp->direct_blocks[i]);
 	ssufs_freeInode(inode);
 	ssufs_writeSuperBlock(superblock);
+	free(superblock);	free(tmp);
 }
 
 int ssufs_open(char *filename){
-	/*3 TODO: create한 파일의 경우만 성공! file handle에서 할당, 초기화. */
+	/*3 MEMO: create한 파일의 경우만 성공! file handle에서 할당, 초기화. */
 	int inode = open_namei(filename);
 	if(inode<0) return -1;	//filename의 파일이 없음
 
@@ -92,38 +93,49 @@ void ssufs_close(int file_handle){
 	file_handle_array[file_handle].inode_number = -1;
 	file_handle_array[file_handle].offset = 0;
 }
-
+//한블럭은 64바이트:
 int ssufs_read(int file_handle, char *buf, int nbytes){
-	/*4 FIXME: 4 
-		! open() buf nb 된 파일의 현재 오프셋에서 요청된 로 요청된 수를 읽음 ytes
-	! 요청된 수는 디스크 상에서 연 nbytes -> 속되지 않을 수도 있음 여러 블록에 걸쳐 있을 수 있음
+	/*4 MEMO: 4 
+	! open()된 파일의 현재 오프셋에서 요청된 buf로 요청된 nbytes수를 읽음
+	! 요청된 수는 디스크 상에서 연속되지 않을 수도 있음 ->여러 블록에 걸쳐 있을 수 있음
 	! 성공 시 리턴 : 0
 	! 에러 시 리턴 : -1
 	! 주의할 점
-	- 파일의 일부 읽기는 지원 하지 않음 요청된 수를 모두 버퍼로 읽 . nbytes , 거나 전혀 읽지 않아야
-	함
-	- 요청된 수를 읽으 nbytes 면 파일 끝을 넘어 가게 되는 경우 아무 것도 읽지 않아야하며 에러를
-	리턴해야 함
-	- 읽기가 블록 경계에서 정렬되지 않고 블록 중간에서 시작하거나 끝날 수 있는 경우 반드시 처리
-	해야 함
+	- 파일의 일부 읽기는 지원 하지 않음 요청된 수를 모두 버퍼로 읽거나 전혀 읽지 않아야 함
+	- 요청된 nbyte수를 읽으면 파일 끝을 넘어 가게 되는 경우 아무 것도 읽지 않아야하며 에러를 리턴해야 함
+	- 읽기가 블록 경계에서 정렬되지 않고 블록 중간에서 시작하거나 끝날 수 있는 경우 반드시 처리 해야 함
 	- 읽기가 성공하면 fd의 오프셋 값도 갱신해야 함
 	*/
+	struct inode_t *tmp = (struct inode_t *) malloc(sizeof(struct inode_t));
+	char bbuf[BLOCKSIZE];
+	ssufs_readInode(file_handle_array[file_handle].inode_number, tmp);
+	int soffset=file_handle_array[file_handle].offset,eoffset=soffset+nbytes,coffset=soffset;
+	int blkoffset,byteoffset,bufoffset;
+	while(coffset<eoffset)
+	{
+		blkoffset=coffset/BLOCKSIZE;
+		ssufs_readDataBlock(blkoffset,bbuf);
+		for(byteoffset=coffset%BLOCKSIZE;byteoffset<BLOCKSIZE;byteoffset++)
+		{
+			if(coffset>=eoffset) {free(tmp);	file_handle_array[file_handle].offset=coffset;	return 0;}
+			else if(blkoffset*BLOCKSIZE+byteoffset>=tmp->file_size) { free(tmp);	file_handle_array[file_handle].offset=coffset;	return -1;}	//파일 끝을 넘음
+			buf[bufoffset++]=bbuf[byteoffset];
+			coffset++;
+		}
+	}
+	return -1;
 }
 
 int ssufs_write(int file_handle, char *buf, int nbytes){
 	/*5 FIXME: 5 
-	! open() buf nb 된 파일의 현재 오프셋에서 요청된 에서 요청된 수를 디스크에 ytes 씀
-	! 요청된 수는 디스크 상에서 연 nbytes -> 속되지 않을 수도 있음 여러 블록에 걸쳐있을 수 있음
+	! open() 된 파일의 현재 오프셋에서 요청된 buf에서 요청된 nbytes수를 디스크에 씀
+	! 요청된 수는 디스크 상에서 연속되지 않을 수도 있음 여러 블록에 걸쳐있을 수 있음
 	! 성공 시 리턴 : 0
 	! 에러 시 리턴 : -1
 	! 주의할 점
-	- 파일의 일부 쓰기는 지원 하지 않음 요청된 모든 수를 파일에 쓰 . nbytes 거나 전혀 쓰지 않아야
-	함
-	- 요청된 수를 nbytes ( : 쓸 수없는 경우 예 요청 된 바이트 수를 쓰면 최대 파일 크기 제한을 초과
-	하거나 쓰기를 완료하는 데 사용할 수 있는 디스크 블록이 없는 경우 free ) 아무것도 쓰지 않고
-	에러 리턴해야 함.
-	- 쓰기가 실퍠한 경우 실패가 발생하기 전에 할당 된 모든 데이터 블록을 해제하고 해당 블록은
-	파일시스템에 반환해야 함. -> 실패한 쓰기는 파일시스템이 시작된 상태와 동일한 상태를 유지
+	- 파일의 일부 쓰기는 지원 하지 않음 요청된 모든 수를 파일에 쓰거나 전혀 쓰지 않아야 함
+	- 요청된 nbytes 수를 쓸 수없는 경우 (ex: 요청 된 바이트 수를 쓰면 최대 파일 크기 제한을 초과하거나 쓰기를 완료하는 데 사용할 수 있는 free 디스크 블록이 없는 경우 ) 아무것도 쓰지 않고 에러 리턴해야 함.
+	- 쓰기가 실퍠한 경우 실패가 발생하기 전에 할당 된 모든 데이터 블록을 해제하고 해당 블록은 파일시스템에 반환해야 함. -> 실패한 쓰기는 파일시스템이 시작된 상태와 동일한 상태를 유지
 	- 쓰기가 블록 경계에서 정렬되지 않고 블록 중간에서 시작하거나 끝날 수 있는 경우 반드시 처리
 	- 쓰기가 성공하면 fd의 오프셋 값도 갱신해야 함
 	*/
